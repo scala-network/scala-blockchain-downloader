02582d2ca6513a731d16fcbce51b153478593dd8
hayzamjs <hayzam@gmail.com>
2021-07-11 09:02:55 +0530

    Don't try to connect to local network peers

--- src/ipfs/root.go
+++ src/ipfs/root.go
@@ -31,6 +31,26 @@ type path struct {
 	path string
 }
 
+
+var defaultServerFilters = []string{
+	"/ip4/10.0.0.0/ipcidr/8",
+	"/ip4/100.64.0.0/ipcidr/10",
+	"/ip4/169.254.0.0/ipcidr/16",
+	"/ip4/172.16.0.0/ipcidr/12",
+	"/ip4/192.0.0.0/ipcidr/24",
+	"/ip4/192.0.2.0/ipcidr/24",
+	"/ip4/192.168.0.0/ipcidr/16",
+	"/ip4/198.18.0.0/ipcidr/15",
+	"/ip4/198.51.100.0/ipcidr/24",
+	"/ip4/203.0.113.0/ipcidr/24",
+	"/ip4/240.0.0.0/ipcidr/4",
+	"/ip6/100::/ipcidr/64",
+	"/ip6/2001:2::/ipcidr/48",
+	"/ip6/2001:db8::/ipcidr/32",
+	"/ip6/fc00::/ipcidr/7",
+	"/ip6/fe80::/ipcidr/10",
+}
+
 func setupPlugins(externalPluginsPath string) error {
 	// Load any external plugins if available on externalPluginsPath
 	plugins, err := loader.NewPluginLoader(filepath.Join(externalPluginsPath, "plugins"))
@@ -62,6 +82,11 @@ func createTempRepo(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
+	cfg.Addresses.NoAnnounce = defaultServerFilters
+	cfg.Swarm.AddrFilters = defaultServerFilters
+	cfg.Discovery.MDNS.Enabled = false
+	cfg.Swarm.DisableNatPortMap = true
 
 	// Create the repo with the config
 	err = fsrepo.Init(repoPath, cfg)
